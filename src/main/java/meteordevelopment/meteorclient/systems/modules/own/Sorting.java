fdbd2fd4a092bf42a55c8ee96b4c33e54d970b9f
Lauorez <48184593+Lauorez@users.noreply.github.com>
2024-03-08 12:37:00 +0100

    Sorting it in man

--- src/main/java/meteordevelopment/meteorclient/systems/modules/own/Sorting.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/own/Sorting.java
@@ -1,31 +1,31 @@
 package meteordevelopment.meteorclient.systems.modules.own;
 
-import meteordevelopment.meteorclient.events.meteor.ActiveModulesChangedEvent;
 import meteordevelopment.meteorclient.events.packets.InventoryEvent;
-import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.player.Rotations;
 import meteordevelopment.orbit.EventHandler;
-import net.minecraft.block.Block;
-import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.block.ChestBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sorting extends Module {
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-    private final Setting<Boolean> autoRotate = sgGeneral.add(new BoolSetting.Builder()
-        .name("rotate")
-        .description("Rotates server-side to the block being mined.")
-        .defaultValue(true)
-        .build()
-    );
+
     private final Setting<Double> range = sgGeneral.add(new DoubleSetting.Builder()
         .name("range")
         .description("The break range.")
@@ -38,16 +38,32 @@ public class Sorting extends Module {
         super(Categories.Own, "Sorting", "Scans nearby chests and tries to stack inventory items into these chests.");
     }
 
+    List<ItemStack> playerInv = new ArrayList<>();
+
     @EventHandler
-    private void onPacketReceive(InventoryEvent event) {
-        System.out.println(event.packet.getContents());
+    private void onInventory(InventoryEvent event) {
+        List<ItemStack> chestInv = event.packet.getContents();
+
+        for (ItemStack itemStack : playerInv) {
+            if (chestInv.contains(itemStack)) {
+                mc.interactionManager.clickSlot(event.packet.getSyncId(), chestInv.indexOf(itemStack), playerInv.indexOf(itemStack), SlotActionType.SWAP, mc.player);
+            }
+        }
+        // Hallo Theo, kommentiere diese Zeile aus, um das Öffnen der Kiste zu sehen. GaLiGrü
+        mc.player.closeHandledScreen();
     }
 
-    @Override
-    public void onActivate() {
-        super.onActivate();
+    @EventHandler
+    private void onPacketSent(PacketEvent.Send event) {
+        System.out.println(event.packet.toString());
+    }
 
+    public void onActivate() {
         if (mc.player == null || mc.world == null) return;
+        for (int i = 0; i < mc.player.getInventory().size(); i++) {
+            playerInv.add(mc.player.getInventory().getStack(i));
+        }
+
         BlockPos playerPos = mc.player.getBlockPos();
         findChestsAroundPlayer(playerPos);
     }
@@ -58,19 +74,14 @@ public class Sorting extends Module {
         int ran = (int) Math.round(doubleValue);
         BlockPos.streamOutwards(playerPos, ran, ran, ran)
             .forEach(pos -> {
-                if (mc.world.getBlockEntity(pos) instanceof ChestBlockEntity) processChest(pos);
+                if (mc.world.getBlockState(pos).getBlock() instanceof ChestBlock) processChest(pos);
             });
     }
 
     private void processChest(BlockPos pos) {
-        if (autoRotate.get()) {
-                mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, new BlockHitResult(new Vec3d(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5), Direction.UP, pos, true));
-        } else {
-            Rotations.rotate(Rotations.getYaw(pos), Rotations.getPitch(pos), () -> {
-                mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, new BlockHitResult(new Vec3d(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5), Direction.UP, pos, true));
-            });
-
-        }
+        pos = BlockPos.ofFloored(pos.getX(), pos.getY(), pos.getZ());
+        Rotations.rotate(Rotations.getYaw(pos), Rotations.getPitch(pos));
+        mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, new BlockHitResult(new Vec3d(pos.getX(), pos.getY(), pos.getZ()), Direction.UP, pos, true));
     }
 
     private void checkItemsAreSame() {
